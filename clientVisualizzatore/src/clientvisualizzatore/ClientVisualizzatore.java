695285c0c5edf59a737af68421617afcba180b8e
mariani_luca <mariani_luca@LABINFO1-03.jeanmonnet.local>
2019-11-28 10:37:13 +0100

    Implementazione client visualizzatore

---
+++ clientVisualizzatore/src/clientvisualizzatore/ClientVisualizzatore.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientvisualizzatore;
+
+/**
+ *
+ * @author mariani_luca
+ */
+public class ClientVisualizzatore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // uhbg8yt8
+    }
+    
+}

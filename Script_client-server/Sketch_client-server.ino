c755690ef6c3ffeeb51929c9cd79598412dc36ae
Brozio01 <47627888+Brozio01@users.noreply.github.com>
2019-11-29 10:57:59 +0100

    Implementate le ultime funzioni
    Invio di valori ogni minuto.
    Metodo attesa per controllare ogni millisecondo se c'è qualcosa nella seriale.
    Se c'è qualcosa, legge la seriale e di conseguenza accende o spegne il led, inviando una conferma.

--- Script_client-server/Sketch_client-server.ino
+++ Script_client-server/Sketch_client-server.ino
@@ -1,28 +1,53 @@
-#include <DHT.h>
+#include <dht.h>
 #define DHTPIN 3
 #define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
+dht DHT;
 
-int button = 2;
-int sensore = 3;
-int temp = 0;
-int umi = 0;
+int button = 2, sensore = 3, led = 4;
+int temp = 0, umi = 0;
 bool statoB = false;
+String str = "";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  temp = dht.readTemperature();
+  int chk = DHT.read11(DHTPIN);
+  temp = DHT.temperature;
   statoB = digitalRead(button);
   
   Serial.print(temp);
   Serial.print(";");
   Serial.print(statoB);
   Serial.print(";");
-  delay(3600);
+  aspetta(60000);
+}
+
+void aspetta(int attesa){
+  int i = 0;
+  
+  while(i < attesa){
+    if (Serial.available())
+      ledManager();
+    else
+      delay(1);
+  }
+}
+
+void ledManager(){
+    str = Serial.read();
+    
+    if (str.equals("lOn")){
+      digitalWrite(led, HIGH);
+      Serial.print("lIsOn");
+    }
+    else if (str.equals("lOf")){
+      digitalWrite(led, LOW);
+      Serial.print("lIsOf");
+    }
 }
